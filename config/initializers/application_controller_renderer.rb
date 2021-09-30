c92bf2094ad51aadd77ff9ef4a3580b65f9e032b
kasu1104kasu <kasumikun1104@gmail.com>
2021-09-30 17:57:20 +0000

    はじめてのプログラミング

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '983e330f6addbea713d4e0e09371b1d1c243403155aa7d5794cea6cf5431a1f2354a79e677925b3c0aaca712bbd010511576c2fd98f03ecedcaab681e67253c0'
